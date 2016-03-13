451c31ce7dca80772bacdbfc837f77c7f974cb05
michawei <freda0330@yahoo.com.tw>
2016-03-13 13:03:36 +0800

    prefixsum-seq.c

--- 10084_Prefix_Sum/prefixsum-seq.c
+++ 10084_Prefix_Sum/prefixsum-seq.c
@@ -2,19 +2,93 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <pthread.h>
 #include "utils.h"
  
 #define MAXN 10000005
 #define MAX_THREAD 4
 uint32_t prefix_sum[MAXN];
+
+struct countData {
+    int left_index;
+    int right_index;
+    uint32_t *prefixSum;
+    uint32_t key;
+};
+typedef countData CountData;
+
+void* threadCountBlock(void *countdata) {
+    CountData blockData = *((CountData*) countdata);
+    uint32_t *prefixSum = blockData.prefixSum;
+    uint32_t sum = 0;
+    int j = 0;
+    for ( int i = blockData.left_index ; i <= blockData.right_index ; i++ ) {
+        sum += encrypt(i, blockData->key);
+        prefixSum[j] = sum;
+        j++;
+    }
+    free(countdata);
+}
+
+void* threadCountAllBlock(void *countdata) {
+    CountData blockData = *((CountData*) countdata);
+    uint32_t *prefixSum = blockData.prefixSum;
+    int j = 0;
+    for ( int i = blockData.left_index ; i <= blockData.right_index ; i++ ) {
+        // sum += (last block sum)
+        prefixSum[j] += blockData->key;
+        j++;
+    }
+    free(countdata);
+}
+
+int MIN(int a, int b) {
+    return (a < b) ? a : b;
+}
+
 int main() {
     int n;
     uint32_t key;
     while (scanf("%d %" PRIu32, &n, &key) == 2) {
+
+        // upper-bound int
+        int BLOCK = (n + (MAX_THREAD-1)) / MAX_THREAD;
+        int count_thread = 0;
+        pthread_t threadList[MAX_THREAD];
+
+        // COUNT BLOCK SUM
+        for ( int i = 1 ; i <= n ; ) {
+            CountData *countdata = (CountData*) malloc(sizeof(CountData));
+            countdata -> left_index = i;
+            countdata -> right_index = MIN(i + BLOCK - 1, n);   // choose (max_index n) or (next-block - 1)
+            countdata -> prefix_sum = prefix_sum + i;
+            countdata -> key = key;
+            i += BLOCK;
+            pthread_creat(&threadList[count_thread], NULL, threadCountBlock, countdata);
+            count_thread++;
+        }
+        for ( int i = 0 ; i < count_thread ; i++ ) {
+            pthread_join(threadList[i], NULL);
+        }
+
         uint32_t sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += encrypt(i, key);
-            prefix_sum[i] = sum;
+        count_thread = 0;
+        // COUNT ALL BLOCK SUM
+        for ( int i = 1 ; i <= n ; ) {
+            CountData *countdata = (CountData*) malloc(sizeof(CountData));
+            countdata -> left_index = i;
+            countdata -> right_index = MIN(i + BLOCK - 1, n);   // choose (max_index n) or (next-block - 1)
+            countdata -> prefix_sum = prefix_sum + i;
+            countdata -> key = sum;
+            i += BLOCK;
+
+            // last block sum
+            sum += prefix_sum[MIN(i + BLOCK - 1, n)];
+            pthread_creat(&threadList[count_thread], NULL, threadCountAllBlock, countdata);
+            count_thread++;
+        }
+        for ( int i = 0 ; i < count_thread ; i++ ) {
+            pthread_join(threadList[i], NULL);
         }
         output(prefix_sum, n);
     }
